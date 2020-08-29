e3a620af2eb04835d3ee6ea55c2b483d19b14608
Norton Pigozzo <nortonpmjr@gmail.com>
2020-08-29 16:19:25 -0500

    Added file moving folder

--- pictureOrganizer/FileHandler.swift
+++ pictureOrganizer/FileHandler.swift
@@ -10,14 +10,63 @@ import Foundation
 
 class FileHandler {
     let manager = FileManager()
+    let consoleIO = ConsoleIO()
+    let testRoot = "/Users/norton/Desktop/Test/"
 
     func createFolders() {
         do {
-            
-         try manager.createDirectory(atPath: "/Users/norton/Desktop/Test/RAW", withIntermediateDirectories: true, attributes: nil)
+            try manager.createDirectory(atPath: "\(testRoot)/RAW", withIntermediateDirectories: true, attributes: nil)
+            try manager.createDirectory(atPath: "\(testRoot)/JPEG", withIntermediateDirectories: true, attributes: nil)
+
+            consoleIO.writeMessage("Folders created sucessfully")
+
+            moveRawPhotos()
+            moveJpegPhotos()
         }
         catch {
-            print("Folder creation errro")
+            consoleIO.writeMessage("Folder creation error", to: .error)
+        }
+    }
+
+    func moveRawPhotos() {
+        //manager.moveitem
+        let photos = searchDirectory()
+        for photo in photos {
+            if photo.contains(".CR2") {
+                do {
+                   try manager.moveItem(atPath: "\(testRoot)/\(photo)", toPath: "\(testRoot)/RAW/\(photo)")
+                }
+                catch {
+                    consoleIO.writeMessage("Error moving raw photos: \(error)", to: .error)
+                }
+            }
+        }
+    }
+
+    func moveJpegPhotos() {
+        //manager.moveitem
+        let photos = searchDirectory()
+        for photo in photos {
+            if photo.contains(".JPG") {
+                do {
+                   try manager.moveItem(atPath: "\(testRoot)/\(photo)", toPath: "\(testRoot)/JPEG/\(photo)")
+                }
+                catch {
+                    consoleIO.writeMessage("Error moving raw photos: \(error)", to: .error)
+                }
+            }
+        }
+    }
+
+    func searchDirectory() -> [String] {
+        do {
+            consoleIO.writeMessage(manager.currentDirectoryPath)
+            let contents = try manager.contentsOfDirectory(atPath: testRoot)
+            return contents
+        }
+        catch {
+            consoleIO.writeMessage("error", to: .error)
+            return []
         }
     }
 }
